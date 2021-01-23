7d18f4a362755c2154eaf28dcc5f8f44197f23d6
GregB712 <gregorybarbas@gmail.com>
2021-01-23 16:55:18 +0100

    Database Removal

--- app.py
+++ app.py
@@ -2,40 +2,29 @@
 # pip install -r requirements.txt
 
 from flask import Flask, render_template, request, redirect
-from flask_mysqldb import MySQL
-import yaml
+import pandas as pd
 
 app = Flask(__name__)
 
-# Configure db
-db = yaml.safe_load(open('db.yaml'))
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
-
-mysql = MySQL(app)
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    queries_df = pd.read_csv("foo.csv")
     if request.method == 'POST':
         # Fetch form data
         qDetails = request.form
         query = qDetails['query']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO searches(query) VALUES(%s)", (query, ))
-        mysql.connection.commit()
-        cur.close()
-        #return 'Success'
+        if (query != ''):
+            single_query = {'Query': query}
+            queries_df = queries_df.append(single_query, ignore_index=True)
+            queries_df.to_csv(r'foo.csv')
         return redirect('/queries')
     return render_template('index.html')
 
 @app.route('/queries')
 def queries():
-    cur = mysql.connection.cursor()
-    resultValue = cur.execute("SELECT * FROM searches")
-    if resultValue > 0:
-        qDetails = cur.fetchall()
+    queries_df = pd.read_csv("foo.csv")
+    if queries_df.size > 2:
+        qDetails = queries_df['Query'].tolist()
         return render_template('queries.html', qDetails=qDetails)
     else :
         return "Empty Database"
