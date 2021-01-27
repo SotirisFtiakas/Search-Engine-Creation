c60d1875996f377b7d577a43e07f5817198788ed
GregB712 <gregorybarbas@gmail.com>
2021-01-27 22:46:36 +0100

    Making page of results form

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Create anaconda env and
 # pip install -r requirements.txt
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 import pandas as pd
 import query_processor as qp
 
@@ -23,15 +23,27 @@ def index():
         return redirect('/queries')
     return render_template('index.html')
 
-@app.route('/queries')
+# @app.route('/queries')
+# def queries():
+#    queries_df = results.head()#pd.read_csv("foo.csv")
+#    if queries_df.size > 2:
+#        qDetailsTitle = queries_df["Title"].tolist()
+#        qDetailsUrl = queries_df["Url"].tolist()
+#        return render_template('queries.html', queryDetails=zip(qDetailsTitle,qDetailsUrl))
+#    else :
+#        return "Empty Database"
+
+@app.route('/queries', methods=['GET', 'POST'])
 def queries():
-    queries_df = results.head()#pd.read_csv("foo.csv")
-    if queries_df.size > 2:
-        qDetailsTitle = queries_df["Title"].tolist()
-        qDetailsUrl = queries_df["Url"].tolist()
-        return render_template('queries.html', queryDetails=zip(qDetailsTitle,qDetailsUrl))
-    else :
-        return "Empty Database"
+    queries_df = results.head()
+    qDetailsTitle = queries_df["Title"].tolist()
+    qDetailsUrl = queries_df["Url"].tolist()
+    if request.method == 'POST':
+        better = request.form.getlist('better')
+        for i in better:
+            print(qDetailsTitle[int(i)])
+    return render_template('queries.html', queryDetails=zip(qDetailsTitle,qDetailsUrl))
+
 
 @app.errorhandler(404)
 def page_not_found(e):
